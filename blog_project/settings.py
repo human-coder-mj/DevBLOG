aea6c8ba24dc014613eaf74bc839e8fff2464403
humancoder1 <jhamanas4532@gmail.com>
2024-07-09 18:23:17 +0530

    Add : Create Users app

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog_app.apps.BlogAppConfig',
+    'users_app.apps.UsersAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
