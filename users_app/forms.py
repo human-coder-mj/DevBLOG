9956b2d3df60c4fc15fce2f2e39d152403f6c2d9
humancoder1 <jhamanas4532@gmail.com>
2024-07-14 17:10:13 +0530

    Remove: Remove two email input field appearing bug

--- users_app/forms.py
+++ users_app/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisteratonForm(UserCreationForm):
-    Email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True)
     
     class Meta:
         model = User
