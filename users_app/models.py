951531e19f857739cd97f4617805ce0007a8e0ea
human-coder-mj <jhamanas4532@gmail.com>
2024-08-10 01:07:53 +0530

    Update: Add Update feature in the User Profile Page

--- users_app/models.py
+++ users_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User , on_delete=models.CASCADE)
@@ -8,3 +9,13 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} Profile"
     
+    def save(self):
+        super().save()
+
+
+        upload_img = Image.open(self.img.path)
+
+        if upload_img.height > 300 or upload_img.width > 300:
+            output_size = (300 , 300)
+            upload_img.thumbnail(output_size)
+            upload_img.save(self.img.path)
