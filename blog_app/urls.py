58da8dc7e2de60f9e0ec0d1461e36841eb8a4363
human-coder-mj <jhamanas4532@gmail.com>
2024-08-11 10:56:46 +0530

    Update: Modify function-based to class-based view for home-page route

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
+from .views import PostListView
 # from django.http import HttpResponse
  
 urlpatterns = [
     # path('', lambda resp:HttpResponse("<h1> Empty-Route </h1>")
     #       , name="empty-route"),
 
-    path('' , views.home , name="blog-home"), #for enabling this as the default page now the first page open will be the home page
+    path('' , PostListView.as_view() , name="blog-home"), #for enabling this as the default page now the first page open will be the home page
 # here the name parameter is the URL pattern name  for the 
     path("about/" , views.about , name="blog-about")
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Posts
+from django.views.generic import ListView , DetailView
 # from django.http import HttpResponse
-# Create your views here.
 
 # def home(request):
 #     return HttpResponse("<h1>Blog-Home</h1>")
@@ -9,15 +9,19 @@ from .models import Posts
 
 
 #Home page view
-def home(request):
-    context = {
-        "posts" : Posts.objects.all()
-    }
-    return render(request , "blog_html_templates/home.html" ,context)
+# def home(request):
+#     context = {
+#         "posts" : Posts.objects.all()
+#     }
+#     return render(request , "blog_html_templates/home.html" ,context)
 
+class PostListView(ListView):
+    model = Posts
+    template_name = "blog_html_templates/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"] #for ordering the blogs according to the date posted , with the newest blog at top
+    pass
 
-
-
-
+#About Page View
 def about(request):
     return render(request , "blog_html_templates/about.html" , {"title" : "About"})
